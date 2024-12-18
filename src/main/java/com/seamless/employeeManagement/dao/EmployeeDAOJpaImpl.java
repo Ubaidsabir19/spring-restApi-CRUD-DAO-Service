6a5e3feeda5effe4fc3b7e55b077fa213f492c01
Ubaid ur rehman <Ubaidsabir19@gmail.com>
2024-12-18 16:24:57 +0500

    crud development

--- src/main/java/com/seamless/employeeManagement/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/seamless/employeeManagement/dao/EmployeeDAOJpaImpl.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
@@ -18,15 +19,24 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> findAll() {
+        TypedQuery<Employee> query = entityManager.createQuery("from Employee", Employee.class);
+        return query.getResultList();
+    }
 
-        // Create the query
-        TypedQuery<Employee> typedQuery = entityManager.createQuery("from Employee", Employee.class);
+    @Override
+    public Employee findById(int id) {
+        return entityManager.find(Employee.class, id);
+    }
 
-        // Execute the query
-        List<Employee> employees = typedQuery.getResultList();
+    @Override
+    public Employee save(Employee employee) {
+        return entityManager.merge(employee);
+    }
 
-        // return the query
-        return employees;
+    @Override
+    public void deleteById(int id) {
+      Employee deleteEmp = entityManager.find(Employee.class, id);
+      entityManager.remove(deleteEmp);
     }
 
 }
